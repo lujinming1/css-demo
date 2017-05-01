affacf8aa299352e8012be3dad985df58c796273
lujinming1 <lujinming163@163.com>
2017-05-01 21:00:09 +0800

    demo1

---
+++ src/Demo1.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './demo1.css'
+class Demo1 extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      curIndex : 1
+    };
+    this.next = this.next.bind(this);
+    this.previous = this.next.bind(this);
+  }
+
+
+  next(){
+    var curPage = ReactDOM.findDOMNode(this.refs.(page + this.state.curIndex));
+    curPage.style.WebkitTransform = "rotateX(-90deg)";
+    var curIndex = this.state.curIndex + 1;
+    this.setState({curIndex: curIndex });
+    var nextPage = ReactDOM.findDOMNode(this.refs.(page + this.state.curIndex));
+    nextPage.style.WebkitTransform = "rotateX(0deg)";
+  }
+
+  previous(){
+
+  }
+  render(){
+
+    return (
+      <div id="demo1">
+      <div id="my3Dpages">
+        <div className="page" id="page1" ref="page1">1</div>
+        <div className="page" id="page2" ref="page2">2</div>
+        <div className="page" id="page3" ref="pgae3">3</div>
+        <div className="page" id="page4" ref="page4">4</div>
+      </div>
+        <div id="op">
+          <a onClick={this.next}>next</a>&nbsp;&nbsp;&nbsp;
+          <a href={this.previous}>previous</a>
+        </div>
+      </div>
+
+    );
+  }
+
+}
+
+export default Demo1;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Demo1 from './Demo1';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <Demo1 />,
+  document.getElementById('Demo1')
+);
