4c3ed5ec6c7be8b116db40579490fc13bb216ac9
lujinming1 <lujinming163@163.com>
2017-05-03 14:18:29 +0800

    next

--- src/Demo1.js
+++ src/Demo1.js
@@ -6,22 +6,26 @@ class Demo1 extends Component {
   constructor(props){
     super(props);
     this.state = {
-      curIndex : 1
+      curIndex : 0
     };
     this.next = this.next.bind(this);
     this.previous = this.next.bind(this);
   }
 
 
-  next(){
-    console.log(this.page);
-  //  var curPage = ReactDOM.findDOMNode(this.refs.page3);
-  var curPage = document.getElementById("page"+this.state.curIndex);
+  next(event){
+    var curIndex = this.state.curIndex;
+
+    var curPage = ReactDOM.findDOMNode(this[`_page${curIndex}`]);
     curPage.style.WebkitTransform = "rotateX(-90deg)";
-    var curIndex = this.state.curIndex + 1;
-    this.setState({curIndex: curIndex });
-    var nextPage = ReactDOM.findDOMNode(this.refs.page2);
+    curIndex = (curIndex + 1)%4;
+    this.setState({curIndex : curIndex });
+
+    var nextPage = ReactDOM.findDOMNode(this[`_page${curIndex}`]);
     nextPage.style.WebkitTransform = "rotateX(0deg)";
+
+    event.stopPropagation();
+    event.preventDefault();
   }
 
   previous(){
@@ -32,10 +36,10 @@ class Demo1 extends Component {
     return (
       <div id="demo1">
       <div id="my3Dpages">
-        <div className="page" id="page1" ref="page1">1</div>
-        <div className="page" id="page2" ref="page2">2</div>
-        <div className="page" id="page3" ref="page3">3</div>
-        <div className="page" id="page4" ref="page4">4</div>
+        <div className="page" id="page1" ref={(c) => this._page0 = c}>1</div>
+        <div className="page" id="page2" ref={(c) => this._page1 = c}>2</div>
+        <div className="page" id="page3" ref={(c) => this._page2 = c}>3</div>
+        <div className="page" id="page4" ref={(c) => this._page3 = c}>4</div>
       </div>
         <div id="op">
           <a onClick={this.next}>next</a>&nbsp;&nbsp;&nbsp;
